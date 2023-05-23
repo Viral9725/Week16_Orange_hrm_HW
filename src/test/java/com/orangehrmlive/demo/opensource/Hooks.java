a4c52005a75a4f1af6a6b2a3405110676f8634e4
viral9725 <viral.kakadiya@gmail.com>
2023-05-23 22:47:41 +0100

    Week16_Orange_hrm_HW

---
+++ src/test/java/com/orangehrmlive/demo/opensource/Hooks.java
@@ -0,0 +1,30 @@
+package com.orangehrmlive.demo.opensource;
+
+import com.cucumber.listener.Reporter;
+import com.orangehrmlive.demo.opensource.propertyreader.PropertyReader;
+import com.orangehrmlive.demo.opensource.utility.Utility;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import java.io.IOException;
+
+public class Hooks extends Utility {
+
+    @Before
+    public void setUp(){selectBrowser(PropertyReader.getInstance().getProperty("browser"));
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+            String screeShotPath = Utility.getScreenshot(driver, scenario.getName().replace(" ", "_"));
+            try {
+                Reporter.addScreenCaptureFromPath(screeShotPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        closeBrowser();
+    }
+}
---
+++ src/test/java/com/orangehrmlive/demo/opensource/steps/BackgroundSteps.java
@@ -0,0 +1,11 @@
+package com.orangehrmlive.demo.opensource.steps;
+
+import cucumber.api.java.en.Given;
+
+public class BackgroundSteps {
+
+    @Given("^I am Login Page$")
+    public void iAmLoginPage() {
+    }
+
+}
